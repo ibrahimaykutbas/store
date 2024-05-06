93656fd710c956daf020ab84add659d5a9f6296d
umutbas <umutbasinfo@gmal.com>
2024-05-06 14:33:17 +0300

    Code edited

--- src/screens/Auth/ForgotPassword.js
+++ src/screens/Auth/ForgotPassword.js
@@ -17,50 +17,34 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import BackIcon from '../../assets/svgs/BackIcon.svg';
-import MailIcon from '../../assets/svgs/mailImage.svg';
 
 const ForgotPassword = ({ navigation }) => {
   const [email, setEmail] = useState('');
-  const [showContent, setShowContent] = useState(false);
 
   return (
     <View style={styles.container}>
-      {showContent ? (
-        <SafeAreaView style={styles.showContent}>
-          <View style={styles.ımageContainer} >
-            <MailIcon width={getRW(110)} height={getRW(110)} />
-          </View>
-          <Text style={styles.showContentText}>
-            We Sent you an Email to reset your password.
-          </Text>
-          <View style={styles.buttonContainer} >
-          <Button title="Return to Login"  onPress={()=> navigation.navigate(routes.LOGIN)}/>
-            </View>
-        </SafeAreaView>
-      ) : (
-        <SafeAreaView>
-          <TouchableOpacity
-            onPress={() => navigation.goBack()}
-            style={styles.backIconContainer}>
-            <BackIcon width={getRW(15)} height={getRW(15)} />
-          </TouchableOpacity>
-          <Text style={styles.title}>Forgot Password</Text>
+      <SafeAreaView>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.backIconContainer}>
+          <BackIcon width={getRW(15)} height={getRW(15)} />
+        </TouchableOpacity>
+        <Text style={styles.title}>Forgot Password</Text>
 
-          <View style={styles.innerContainer}>
-            <Input
-              value={email}
-              onChangeText={setEmail}
-              placeholder="Enter Email address"
-            />
+        <View style={styles.innerContainer}>
+          <Input
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Enter Email address"
+          />
 
-            <Button
-              title="Contine"
-              onPress={() => setShowContent(true)}
-              disabled={email == ''}
-            />
-          </View>
-        </SafeAreaView>
-      )}
+          <Button
+            title="Contine"
+            onPress={() => navigation.navigate(routes.EMAIl_SEND)}
+            disabled={email == ''}
+          />
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
@@ -94,30 +78,4 @@ export const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: getRH(23),
   },
-  showContent: {
-    flex: 1,
-    backgroundColor: Colors.WHITE,
-    marginHorizontal:getRH(24)
-  },
-  /* Shadow efekti yazının altında olmalı  */
-  showContentText: {
-    color: Colors.BLACK,
-    fontSize:Fonts.size(27),
-    fontWeight:'900',
-    textAlign:'center',
-    textShadowOffset: { width: 0, height:6 },
-    shadowOpacity:0.3,
-    shadowRadius:2
-  },
-  ımageContainer:{
-    justifyContent:'center',
-    alignItems:'center',
-    marginTop:getRH(292),
-    marginBottom:getRH(32)
-  },
-  buttonContainer:{
-    justifyContent:'center',
-    alignSelf:'center',
-    width:getRH(200)
-  }
 });
