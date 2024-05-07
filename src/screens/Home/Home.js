69a671ab4ddf0467983fb84534de412e78dc4f67
ibrahimaykutbas <ibrahimaykutbas@gmail.com>
2024-05-07 20:59:30 +0300

    Add Header and SearchBar

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, ScrollView } from 'react-native';
 import React from 'react';
 
-const Home = () => {
+import Colors from '../../theme/Colors';
+import { getRW, getRH } from '../../theme/Units';
+
+import Header from '../../components/Header';
+import SearchBar from '../../components/SearchBar';
+
+import routes from '../../navigation/routes';
+import Categories from '../../components/Categories';
+
+const Home = ({ navigation }) => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header isHome onPressBasket={() => console.log('Basket')} />
+      <SearchBar />
+      <ScrollView style={styles.content}>
+       <Categories/>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.WHITE,
+  },
+  content: {
+    flex: 1,
+    marginHorizontal: getRW(24),
+  },
+});
